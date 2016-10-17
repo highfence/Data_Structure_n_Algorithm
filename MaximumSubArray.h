286a052601aae4393ca4baac375165722e35a80d
highfence <highfence@naver.com>
2016-10-18 03:36:55 +0900

    Finish Brute way

--- MaximumSubArray.h
+++ MaximumSubArray.h
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <stdlib.h>
+
 using namespace std;
+
+const int RAND_RANGE = 100;
+const int RAND_MINUS_VAL = 50;
+const int MAX_ALLOCATE = 50;
+const int INIT_MINUS_INFINIT = -999999;
 
 typedef struct idxAndSum
 {
 	int startIdx;
 	int endIdx;
 	int sumVal;
-};
+}idxAndSum;
+
+
+
 
 
 // Funtions
+void printResult(idxAndSum*, int*, int);
+int* makeRandArray(int);
 idxAndSum* BruteWayFunc(int*, int);
+void testInterface();
+
--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -9,12 +9,64 @@
 
 
 
-
 int main(void)
 {
-
+	while (1)
+	{
+		testInterface();
+	}
 
 	return 0;
+}
+
+
+
+/*
+	printResult
+	결과값을 가진 구조체 idxAndSum의 주소값을 받아 출력해주는 함수.
+*/
+
+void printResult(idxAndSum* input, int* arr, int size)
+{
+	if (!size) return;
+	cout << "입력 배열 : { ";
+
+	for (int i = 0; i < size; ++i)
+	{
+		cout << arr[i];
+		if (i != size - 1)
+		{
+			cout << ", ";
+		}
+	}
+	cout << " } " <<endl;
+	cout << "시작인덱스 : " << input->startIdx << " 끝 인덱스 : " << input->endIdx << " 결과 값 : " << input->sumVal << endl;
+	return;
+}
+
+
+/*
+	makeRandArray
+	size를 받아 배열을 만들고 그 안에 랜덤한 int값을 넣어주는 함수.
+*/
+
+int* makeRandArray(int size)
+{
+	int* returnArr;
+	int i;
+
+	if (!size)
+	{
+		return NULL;
+	}
+
+	returnArr = new int[size];
+	for (i = 0; i < size; ++i)
+	{
+		returnArr[i] = rand() % RAND_RANGE - RAND_MINUS_VAL;
+	}
+
+	return returnArr;
 }
 
 
@@ -44,14 +96,14 @@ idxAndSum* BruteWayFunc(int* arr, int size)
 	// Exception handling
 	if (!arr) {
 		cout << "There is a problem in input Array. \n";
-		return;
+		return NULL;
 	}
 
 	// Setting returnStruct and initializing.
-	returnStruct = (idxAndSum*)malloc(sizeof(idxAndSum));
+	returnStruct = new idxAndSum;
 	returnStruct->startIdx = 0;
 	returnStruct->endIdx = 0;
-	returnStruct->sumVal = 0;
+	returnStruct->sumVal = INIT_MINUS_INFINIT;
 
 	for (i = 0; i < size; ++i)
 	{
@@ -70,4 +122,33 @@ idxAndSum* BruteWayFunc(int* arr, int size)
 
 	return returnStruct;
 
+}
+
+/*
+	testInterface
+	테스트를 쉽게 하기 위하여 제공하는 함수.
+*/
+
+void testInterface(void)
+{
+	int size;
+	int* arr;
+	idxAndSum* result;
+
+	cout << "길이가 몇 인 배열을 만듭니까? (exit for -1) : ";
+	cin >> size;
+	
+	if (size == -1) {
+		exit(1);
+	}
+
+	arr = makeRandArray(size);
+	result = BruteWayFunc(arr, size);
+	printResult(result, arr, size);
+	cout << endl;
+
+	delete result;
+	delete[] arr;
+
+	return;
 }
